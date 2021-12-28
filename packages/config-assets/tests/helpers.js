e9fb2f4a7add19a19927df8e62b062a869922737
beefchimi <curtdulmage@gmail.com>
2021-12-27 20:33:45 -0500

    :construction_worker: [Jest] Get tests working

--- packages/config-assets/tests/helpers.js
+++ packages/config-assets/tests/helpers.js
@@ -2,7 +2,7 @@ import {readFileSync} from 'fs';
 import {resolve as resolvePath} from 'path';
 
 // TODO: Should we detect if a file extension is provided?
-export function getFixture(fileName: string) {
+export function getFixture(fileName) {
   return readFileSync(
     resolvePath(__dirname, 'fixtures', `${fileName}.svg`),
     'utf8',
